78b97d0e3b7eceac58311114703676b975e2098a
agarciaia <ag.analisis247@gmail.com>
2026-09-14 00:06:53 -0300

    Connect Supabase profile login: service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'bellamujer-v1';
+const CACHE_NAME = 'bellamujer-v2-profile';
 const APP_SHELL = [
   './',
   './index.html',
@@ -22,6 +22,8 @@ self.addEventListener('activate', event => {
 
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
+  const requestUrl = new URL(event.request.url);
+  if (requestUrl.origin !== self.location.origin) return;
   event.respondWith(
     fetch(event.request)
       .then(response => {
